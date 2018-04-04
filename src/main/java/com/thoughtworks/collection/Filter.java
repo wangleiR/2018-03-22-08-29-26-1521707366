1c7cb7aa40ccf705c2c5fb15dda41921d5daf096
wangleiR <1220632559@qq.com>
2018-04-04 18:40:08 +0800

    Filter

--- src/main/java/com/thoughtworks/collection/Filter.java
+++ src/main/java/com/thoughtworks/collection/Filter.java
@@ -2,8 +2,11 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Filter {
 
@@ -13,19 +16,23 @@ public class Filter {
      this.array = array;
     }
 
+    //求数组中的偶数 -ok
     public List<Integer> filterEven() {
-        throw new NotImplementedException();
+        return array.stream().filter(n -> n%2 == 0).collect(Collectors.toList());
     }
-
+    //求数组中3的倍数 -ok
     public List<Integer> filterMultipleOfThree() {
-        throw new NotImplementedException();
+        return array.stream().filter(n -> n%3 == 0).collect(Collectors.toList());
     }
-
+    //求两数组的公共元素
     public List<Integer> getCommonElements(List<Integer> firstList, List<Integer> secondList) {
-        throw new NotImplementedException();
+        List m1 = new ArrayList(firstList);
+        List m2 = new ArrayList(secondList);
+        m1.retainAll(m2);
+        return  m1;
     }
-
+    //去除数组中的重复元素 -ok
     public List<Integer> getDifferentElements() {
-        throw new NotImplementedException();
+        return array.stream().distinct().collect(Collectors.toList());
     }
 }
