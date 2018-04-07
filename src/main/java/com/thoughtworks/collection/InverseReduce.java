7ea7e3c017a034d996cd5b97abfb8c38290ae431
wangleiR <1220632559@qq.com>
2018-04-07 19:19:41 +0800

    InverseReduce.java

--- src/main/java/com/thoughtworks/collection/InverseReduce.java
+++ src/main/java/com/thoughtworks/collection/InverseReduce.java
@@ -2,6 +2,7 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
@@ -14,8 +15,13 @@ public class InverseReduce {
     public InverseReduce(Random random) {
         this.random = random;
     }
-
+    //根据给定数字无限分割至等于0(提示：9-2=7,7-2=5 ...) -ok
     public List<Integer> divideToSmaller(int number) {
-        throw new NotImplementedException();
+        List<Integer> res = new ArrayList<>();
+        while (number -2> 0){
+            number -= 2;
+            res.add(number);
+        }
+        return res;
     }
 }
